68008c41f6cac0a45cba5cf4ff349efaa1ae29c9
Anisha Nagarajan <yourgithubemail@example.com>
2026-01-07 11:47:46 +0530

    Initial Flask PaaS app

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello Students! PaaS demo is working 🚀"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
